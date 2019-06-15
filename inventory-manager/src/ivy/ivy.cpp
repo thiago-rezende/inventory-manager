36230c831a3656497622d4b6a137ccf57c745ebc
Novout <novout@hotmail.com>
2019-06-15 01:28:03 -0300

    Classe IvyManager feita

--- inventory-manager/src/ivy/ivy.cpp
+++ inventory-manager/src/ivy/ivy.cpp
@@ -1,1 +1,39 @@
 #include "ivy.hpp"
+
+namespace ivy
+{
+IvyManager::IvyManager()
+{
+}
+
+IvyManager::~IvyManager()
+{
+}
+
+void IvyManager::cadastrar()
+{
+}
+
+void IvyManager::editar(std::string nNome)
+{
+}
+
+int IvyManager::buscar(std::string nNome)
+{
+}
+
+int IvyManager::buscar(float nPreco)
+{
+}
+
+int IvyManager::buscar(int nQuantidade)
+{
+}
+
+void IvyManager::vender(int nId, int nQuantidade)
+{
+}
+
+void IvyManager::comprar(int nid, int nQuantidade)
+{
+}
--- inventory-manager/src/ivy/ivy.hpp
+++ inventory-manager/src/ivy/ivy.hpp
@@ -6,6 +6,18 @@ namespace ivy
 class IvyManager
 {
 private:
+    ivy::Vetor m_estoque;
+
 public:
+    IvyManager();
+    ~IvyManager();
+
+    void cadastrar();
+    void editar(std::string nNome);
+    int buscar(std::string nNome);
+    int buscar(float nPreco);
+    int buscar(int nQuantidade);
+    void vender(int nId, int nQuantidade);
+    void comprar(int nid, int nQuantidade);
 };
 } // namespace ivy
