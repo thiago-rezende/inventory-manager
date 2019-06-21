8b92c5adc2b5dc4b00deb22bd82ab0890325e9a1
Thiago Rezende <bitrateti@gmail.com>
2019-06-21 06:12:21 -0300

    adicionada a opcao de alternar entre GUI e CLI

--- src/main.cpp
+++ src/main.cpp
@@ -7,46 +7,58 @@
 
 int main(int argc, char **argv)
 {
-    /* ivy::IvyManager ivy;
-    ivy::InterfaceCLI cli(ivy);
-    cli.run(); */
+    if ((std::string)argv[1] == "-cli")
+    {
+        ivy::IvyManager ivy;
+        ivy::InterfaceCLI cli(ivy);
+        cli.run();
+    }
+    else
+    {
+        /* Define widgets */
+        nana::form fm{nana::API::make_center(300, 200)};
+        nana::textbox usr{fm}, pswd{fm};
+        nana::button login{fm, "Login"}, cancel{fm, "Cancel"};
 
-    /* Define widgets */
-    nana::form fm;
-    nana::textbox usr{fm}, pswd{fm};
-    nana::button login{fm, "Login"}, cancel{fm, "Cancel"};
+        login.events().click([&usr, &pswd]() {
+            std::string usr_str;
+            std::string usr_pswd;
+            usr.getline(0, usr_str);
+            pswd.getline(0, usr_pswd);
+            nana::msgbox m("Hello");
+            m << usr_str << "\n"
+              << usr_pswd;
+            m();
+        });
 
-    login.events().click([&fm] {
-        std::cout << "Button Clicked!" << std::endl;
-    });
+        cancel.events().click([&fm] {
+            fm.close();
+        });
 
-    cancel.events().click([&fm] {
-        fm.close();
-    });
+        usr.tip_string("User:").multi_lines(false);
+        pswd.tip_string("Password:").multi_lines(false).mask('*');
 
-    usr.tip_string("User:").multi_lines(false);
-    pswd.tip_string("Password:").multi_lines(false).mask('*');
+        /* Define a place for the form. */
+        nana::place plc{fm};
 
-    /* Define a place for the form. */
-    nana::place plc{fm};
+        /* Divide the form into fields */
+        plc.div("<><weight=80% vertical<><weight=70% vertical <vertical gap=10 textboxes arrange=[25,25]>  <weight=25 gap=10 buttons> ><>><>");
 
-    /* Divide the form into fields */
-    plc.div("<><weight=80% vertical<><weight=70% vertical <vertical gap=10 textboxes arrange=[25,25]>  <weight=25 gap=10 buttons> ><>><>");
+        /* Insert widgets */
 
-    /* Insert widgets */
+        /* The field textboxes is vertical. It automatically adjusts the widgets' top and height attributes. */
+        plc.field("textboxes") << usr << pswd;
 
-    /* The field textboxes is vertical. It automatically adjusts the widgets' top and height attributes. */
-    plc.field("textboxes") << usr << pswd;
+        plc["buttons"] << login << cancel;
 
-    plc["buttons"] << login << cancel;
+        /* Finially, the widgets should be collocated.
+        Do not miss this line! Otherwise, the widgets will not be collocated
+        until the form is resized. */
+        plc.collocate();
 
-    /* Finially, the widgets should be collocated.
-    Do not miss this line! Otherwise, the widgets will not be collocated
-    until the form is resized. */
-    plc.collocate();
-
-    fm.show();
-    nana::exec();
+        fm.show();
+        nana::exec();
+    }
 
     return 0;
 }
