7501e70f7e2e732d2cb600d7e05bbd316092dfcb
Novout <novout@hotmail.com>
2019-06-28 00:03:02 -0300

    Interface Principal GUI Inicial

--- src/main.cpp
+++ src/main.cpp
@@ -36,157 +36,198 @@ int main(int argc, const char **argv)
     else
     {
         /**
-         * @brief Criacao do form fm abrindo a tela inicialmente com 1000x900
-         * 
+         * @brief Criacao do form fm_init
+         * @param make_center(1000x900)
+         *
          */
-        nana::form fm{nana::API::make_center(1000, 900)};
+        nana::form fm_init{nana::API::make_center(1000, 900)};
 
         /**
-         * @brief Nome do ivymanager.exe
-         * 
-         */
-        fm.caption(("IvyManager"));
+        * @brief Nome do ivymanager.exe
+        *
+        */
+        fm_init.caption(("IvyManager"));
 
         /**
-         * @brief Criacao dos textos de input : nome, marca, valor, Id e quantidade
+         * @brief Criacao do label cabecalho_ivy e estilizacao do texto 'IvyManager'
          * 
          */
-        nana::textbox innome{fm}, inmarca{fm}, invalor{fm}, inid{fm}, inquantidade{fm};
+        nana::label cabecalho_ivy{fm_init, ""};
+        cabecalho_ivy.format(true);
+        cabecalho_ivy.caption("<bold=true, color=0xff0000, font=\"Consolas\" size=18 center>IvyManager</>");
+        cabecalho_ivy.text_align(nana::align::center, nana::align_v::center);
 
         /**
-         * @brief Criacao dos botoes de enviar e cancelar o cadastro de produto
-         * 
+         * @brief Grupo geral grp_geral_principal
+         *
          */
-        nana::button env{fm, "Enviar"}, cancel{fm, "Cancelar"};
+        nana::group grp_geral_principal{fm_init, "", true};
 
         /**
-         * @brief Criacao dos botoes de buscar, editar, comprar ,vender
+         * @brief Place plc_principal
          * 
+         * @return nana::place 
          */
-        nana::button buscar{fm, "Buscar"}, editar{fm, "Editar"}, comprar{fm, "Comprar"}, vender{fm, "Vender"};
+        nana::place plc_principal(fm_init);
 
         /**
-         * @brief Criacao da escolha de garantia (Sim ou Nao)
+         * @brief Divisao contento as dimensoes do cabecalho geral_principal e do grupo grp_geral_principal
+         * @param 'vert gap=10 margin=5' colocando na vertical,espacamento de 10 e margem de 5
+         * @param <lab_grupo weight=30> Separacao do geral_principal e grp_geral_principal
+         * @param <todo> Para exibicao do grp_geral_principal,respeitando as condicoes de 'lab'
          * 
          */
-        nana::combox ingarantia(fm, nana::rectangle(20, 3, 150, 30), "Garantia");
+        plc_principal.div("vert gap=10 margin=5 <lab_grupo weight=30><todo>");
+        plc_principal["lab_grupo"] << cabecalho_ivy;
+        plc_principal["todo"] << grp_geral_principal;
 
         /**
-         * @brief Menu de escolha de garantia
+         * @brief Grupo de produtos(a esquerda), e grupo de botoes(a direta)
          * 
          */
-        ingarantia.caption(("Possui Garantia?"));
-        ingarantia.push_back("Sim");
-        ingarantia.push_back("Nao");
+        nana::group grp_tela_produto{grp_geral_principal, ("<bold=true, color=0xff0000, font=\"Consolas\">LISTA DE PRODUTOS:</>"), true};
+        nana::group grp_btn_direita{grp_geral_principal, ("<bold=true, color=0xff0000, font=\"Consolas\">BOTOES:</>"), true};
+        grp_geral_principal.div("horizontal gap=3 margin=20  < <tela_esquerda margin=20> | 30% <grupo_direita>> ");
+        grp_geral_principal["tela_esquerda"] << grp_tela_produto;
+        grp_geral_principal["grupo_direita"] << grp_btn_direita;
 
-        /**
-         * @brief Base para o Input dos dados de um novo produto
-         * 
-         */
-        innome.tip_string("Nome:").multi_lines(false);
-        inmarca.tip_string("Marca:").multi_lines(false);
-        invalor.tip_string("Valor:").multi_lines(false);
-        inid.tip_string("Id:").multi_lines(false);
-        inquantidade.tip_string("Quantidade:").multi_lines(false);
+        grp_tela_produto.div("vert gap=5 margin=3");
 
+        /* grp_btn_direita.div( "<vertical margin=2 gap= 2 <vert lab> | 40% < <left_field> | 70% <right_field>> >");*/
         /**
-         * @brief Base plc do form
-         * 
-         */
-        nana::place plc{fm};
+        * @brief Tela de cadastro,acionada assim que o btn_cadastrar for clicado
+        * @param form fm_init
+        *
+        */
+        fm_init.events().click([&fm_init] { //No momento nao foi implementado
+            /**
+             * @brief Criacao do form de cadastro
+             *
+             */
+            nana::form fm_cadastro{nana::API::make_center(1000, 900)}, (fm_init);
 
-        /**
-         * @brief Cabecalho do Cadastro
-         * 
-         */
-        nana::label input{fm, ""};
-        input.format(true);
-        input.caption("<bold color=0x0080FF size=16 center>Cadastrar Produto</>");
+            /**
+             * @brief Nome da tela de cadastro
+             *
+             */
+            fm_cadastro.caption(("IvyManager - Cadastrar"));
 
-        /**
-         * @brief Grupo dos botoes buscar, editar, comprar e vender
-         * 
-         */
-        nana::group buttons{fm, "<bold=true, color=blue>Menu:</>", true};
+            /**
+             * @brief Criacao dos textos de input : nome, marca, valor, Id e quantidade
+             *
+             */
+            nana::textbox innome{fm_cadastro}, inmarca{fm_cadastro}, invalor{fm_cadastro}, inid{fm_cadastro}, inquantidade{fm_cadastro};
 
-        /**
-         * @brief Divisao contento as dimensoes da interface de cadastro
-         * @param <><weight=70% vertical<> Larguta da divisao
-         * @param <weight=37% vertical> Deslocamento da altura entre o formulário e os botoes do grupo buttons
-         * @param <vertical gap=10 textboxs arrange=[50,25,25,25,25,25,25]> Arrange determinando o tamanho de cada textbox dos input.
-         *        Gap determinando a separação de cada input
-         * @param <weight=40 gap=60 buttons> Tamanho. Separacao dos botoes de enviar e cancelar dos input de cadastro
-         * @param <todo weight=70 margin=20 gap=10> Barra das opcoes de buscar, editar, comprar, vender
-         * 
-         */
-        plc.div("<><weight=70% vertical<><weight=37% vertical <vertical gap=10 textboxs arrange=[50,25,25,25,25,25,25]> <weight=40 gap=60 buttons> ><todo weight=70 margin=20 gap=10><>><>");
+            /**
+             * @brief Criacao dos botoes de enviar e cancelar o cadastro de produto
+             *
+             */
+            nana::button env{fm_cadastro, "Enviar"}, cancel{fm_cadastro, "Cancelar"};
 
-        /**
-         * @brief Coloca a barra "todo" na base plc
-         * 
-         */
-        plc["todo"] << buscar << editar << comprar << vender;
+            /**
+             * @brief Criacao dos botoes de buscar, editar, comprar ,vender
+             *
+             */
+            nana::button buscar{fm_cadastro, "Buscar"}, editar{fm_cadastro, "Editar"}, comprar{fm_cadastro, "Comprar"}, vender{fm_cadastro, "Vender"};
 
-        /**
-         * @brief Coloca os input de produtos na base plc
-         * 
-         */
-        plc.field("textboxs") << input << innome << inmarca << invalor << inid << inquantidade << ingarantia;
+            /**
+             * @brief Criacao da escolha de garantia (Sim ou Nao)
+             * 
+             */
+            nana::combox ingarantia(fm_cadastro, nana::rectangle(20, 3, 150, 30), "Garantia");
 
-        /**
-         * @brief Coloca os botoes de enviar e cancelar o input na base plc
-         * 
-         */
-        plc.field("buttons") << env << cancel;
-        /**
-         * @brief Coloca o place plc
-         * 
-         */
-        plc.collocate();
+            /**
+             * @brief Menu de escolha de garantia
+             * 
+             */
+            ingarantia.caption(("Possui Garantia?"));
+            ingarantia.push_back("Sim");
+            ingarantia.push_back("Nao");
 
-        /**
-         * @brief Exibe o form fm
-         * 
-         */
-        fm.show();
+            /**
+             * @brief Base para o Input dos dados de um novo produto
+             * 
+             */
+            innome.tip_string("Nome:").multi_lines(false);
+            inmarca.tip_string("Marca:").multi_lines(false);
+            invalor.tip_string("Valor:").multi_lines(false);
+            inid.tip_string("Id:").multi_lines(false);
+            inquantidade.tip_string("Quantidade:").multi_lines(false);
 
-        /**
-         * @brief Busca a imagem de fundo na pasta res
-         * 
-         */
-        nana::paint::image img("res/background.bmp");
+            /**
+             * @brief Base plc do form
+             * 
+             */
+            nana::place plc{fm_cadastro};
 
-        /**
-         * @brief Chama a funcao de desenhar
-         * 
-         * @return nana::drawing 
-         */
-        nana::drawing dw(fm);
+            /**
+             * @brief Cabecalho do Cadastro
+             * 
+             */
+            nana::label input{fm_cadastro, ""};
+            input.format(true);
+            input.caption("<bold color=0x0080FF size=16 center>Cadastrar Produto</>");
 
-        /**
-         * @brief Pega a imagem de fundo e coloca no dw
-         * @param Grafico graph
-         * 
-         */
-        dw.draw([&img](nana::paint::graphics &graph) {
-            if (img.empty())
-            {
-                std::cout << "IMAGEM DE FUNDO NAO EXISTE!" << std::endl;
-                return;
-            }
-            img.paste(graph, nana::point{});
+            /**
+             * @brief Grupo dos botoes buscar, editar, comprar e vender
+             * 
+             */
+            nana::group buttons{fm_cadastro, "<bold=true, color=blue>Menu:</>", true};
+
+            /**
+             * @brief Divisao contento as dimensoes da interface de cadastro
+             * @param <><weight=70% vertical<> Larguta da divisao
+             * @param <weight=37% vertical> Deslocamento da altura entre o formulário e os botoes do grupo buttons
+             * @param <vertical gap=10 textboxs arrange=[50,25,25,25,25,25,25]> Arrange determinando o tamanho de cada textbox dos input.
+             *        Gap determinando a separação de cada input
+             * @param <weight=40 gap=60 buttons> Tamanho. Separacao dos botoes de enviar e cancelar dos input de cadastro
+             * @param <todo weight=70 margin=20 gap=10> Barra das opcoes de buscar, editar, comprar, vender
+             * 
+             */
+            plc.div("<><weight=70% vertical<><weight=37% vertical <vertical gap=10 textboxs arrange=[50,25,25,25,25,25,25]> <weight=40 gap=60 buttons> ><todo weight=70 margin=20 gap=10><>><>");
+
+            /**
+             * @brief Coloca a barra "todo" na base plc
+             * 
+             */
+            plc["todo"] << buscar << editar << comprar << vender;
+
+            /**
+             * @brief Coloca os input de produtos na base plc
+             * 
+             */
+            plc.field("textboxs") << input << innome << inmarca << invalor << inid << inquantidade << ingarantia;
+
+            /**
+             * @brief Coloca os botoes de enviar e cancelar o input na base plc
+             * 
+             */
+            plc.field("buttons") << env << cancel;
+            /**
+             * @brief Coloca o place plc
+             * 
+             */
+            plc.collocate();
+
+            /**
+             * @brief Exibe o form fm
+             * 
+             */
+            fm_init.show();
+
+            /**
+             * @brief fm_modal entra em rotina
+             * 
+             */
+            fm_cadastro.modality();
+
+            /**
+             * @brief Constroi um novo nana::exec objeto
+             * 
+             */
         });
-
-        /**
-         * @brief Mostra a imagem de fundo
-         * 
-         */
-        dw.update();
-
-        /**
-         * @brief Constroi um novo nana::exec objeto
-         * 
-         */
+        plc_principal.collocate();
+        fm_init.show();
         nana::exec();
     }
 
